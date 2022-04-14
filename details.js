aae00a49cad25c60826662ba03c62fec0b97a08e
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-04-14 21:41:46 +0300

    task completed

---
+++ details.js
@@ -0,0 +1,119 @@
+`use strict`;
+
+window.onload = async () => {
+  const countryName = new URLSearchParams(window.location.search).get("name");
+  const response = await fetch(
+    "https://restcountries.com/v2/name/" + countryName
+  );
+  const countries = await response.json();
+  const country = countries[0];
+  console.log(country);
+  displayDetails(country);
+
+  displayBorderCountries();
+};
+
+const displayBorderCountries = () => {
+  const borders = document.querySelector("ul.flex-wrap");
+  borders.addEventListener("click", async (e) => {
+    const query = e.target.innerText;
+    const response = await fetch("https://restcountries.com/v2/alpha/" + query);
+    const country = await response.json();
+    displayDetails(country);
+  });
+};
+
+//* dark/light mode
+const darkmode = document.querySelector(".dark-mode");
+const toDarkMode = () => {
+  const header = document.querySelector(`header`);
+  header.classList.add("text-light", "bg-header");
+  const body = document.querySelector("body");
+  body.style.backgroundColor = `hsl(207, 26%, 17%)`;
+  const main = document.querySelector("main > div > div");
+  main.classList.add("text-light");
+  lightmode.classList.remove("d-none");
+  darkmode.classList.add("d-none");
+  const backBtn = document.querySelector("section.container > a");
+  backBtn.classList.remove("bg-white");
+  backBtn.classList.add("text-white");
+  const bordersBtn = document.querySelectorAll("li button.rounded");
+  bordersBtn.forEach((el) => {
+    el.classList.remove("bg-white");
+    el.classList.add("text-white");
+  });
+};
+const lightmode = document.querySelector(".light-mode");
+const toLightMode = () => {
+  const header = document.querySelector(`header`);
+  header.classList.remove("text-light", "bg-header");
+  const body = document.querySelector("body");
+  body.style.backgroundColor = `hsl(0, 0%, 98%)`;
+  const main = document.querySelector("main > div > div");
+  main.classList.remove("text-light");
+  lightmode.classList.add("d-none");
+  darkmode.classList.remove("d-none");
+  const backBtn = document.querySelector("section.container > a");
+  backBtn.classList.remove("bg-white");
+  backBtn.classList.remove("text-white");
+  backBtn.classList.add("bg-white");
+  backBtn.classList.remove("text-white");
+  const bordersBtn = document.querySelectorAll("li button.rounded");
+  bordersBtn.forEach((el) => {
+    el.classList.add("bg-white");
+    el.classList.remove("text-white");
+  });
+};
+darkmode.addEventListener("click", toDarkMode);
+lightmode.addEventListener("click", toLightMode);
+
+const displayDetails = (country) => {
+  const img = document.querySelector(`.col-12 > img`);
+  img.src = country.flags.png;
+  const col = document.querySelector(".details");
+
+  col.innerHTML = ``;
+
+  const lang = country.languages.map((el) => el.name);
+
+  col.innerHTML += `<h2 class=" fw-bold">${country.name}</h2>
+        <div class="d-flex mb-3 justify content-between">
+              <ul class="list-unstyled d-flex flex-column gap-2">
+                <li> <strong>Native Name:</strong> ${country.nativeName}</li>
+                <li><strong>Population</strong>:${country.population.toLocaleString(
+                  "en-US"
+                )}</li>
+                <li><strong>Region</strong>: ${country.region}</li>
+                <li><strong>Sub-Region</strong>: ${country.subregion}</li>
+                <li><strong>Capital</strong>: ${country.capital}</li>
+              </ul>
+              <ul class="list-unstyled">
+                <li><strong>Top Level Domain</strong>: ${
+                  country.topLevelDomain[0]
+                }</li>
+                <li><strong>Currencies</strong>: ${
+                  country.currencies[0].name
+                }</li>
+
+                <li><strong>languages</strong>: ${lang.join(",")}</li>
+              </ul>
+            </div>
+            `;
+  const div = document.createElement("div");
+  div.classList.add("d-flex", "gap-2", "flex-wrap", "align-items-center");
+  const p = document.createElement("p");
+  p.innerHTML = `<strong>Border countries:</strong>`;
+  div.appendChild(p);
+  const ul = document.createElement("ul");
+  ul.classList.add("d-flex", "gap-2", "ps-0", "list-unstyled", "flex-wrap");
+  country.borders.map((el) => {
+    const li = document.createElement("li");
+    li.classList.add("borders");
+    li.innerHTML = `<button class="btn py-1 px-4 rounded bg-white btn-shadow">
+        ${el}
+      </button>`;
+    ul.appendChild(li);
+  });
+  div.appendChild(ul);
+  col.appendChild(div);
+};
--- index.js
+++ index.js
@@ -16,49 +16,158 @@ const displayCountries = async (url) => {
     const row = document.querySelector(`.row`);
     row.innerHTML = ``;
     countries.forEach((country) => {
-      row.innerHTML += displayCards(country);
+      if (country.name.common) {
+        row.innerHTML += displayCardsByRegion(country);
+      } else {
+        row.innerHTML += displayCards(country);
+      }
     });
   } catch (error) {
     console.log(error);
   }
 };
 const input = document.querySelector(`input`);
-input.onchange = () => {
+input.onkeyup = () => {
   const query = input.value;
   const countryNameUrl = "https://restcountries.com/v2/name/" + query;
-  if (query.length > 2) {
-    displayCountries(countryNameUrl);
-  } else {
+  if (query.length < 3) {
     displayCountries(allCountriesUrl);
+  } else {
+    displayCountries(countryNameUrl);
   }
 };
-const africa = document.querySelector(`.africa`);
-africa.addEventListener(`click`, () => {
-  console.log(africa.textContent);
+
+document.querySelector(`ul`).addEventListener(`click`, function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains(`dropdown-item`)) {
+    const region = e.target.textContent;
+    const regionUrl =
+      "https://restcountries.com/v3.1/region/" + region.toLowerCase();
+    displayCountries(regionUrl);
+  }
 });
-const america = document.querySelector(`.america`);
-const asia = document.querySelector(`.asia`);
-const oceania = document.querySelector(`.oceania`);
-const europe = document.querySelector(`.europe`);
-const innerTextName = (region) => {
-  region.onclick = (el) => el.innerText;
-};
-innerTextName(africa);
+
 const displayCards = (country) => {
   return ` <div class="col-12 mb-3 col-sm-6 col-lg-3">
   <div class="card">
+   <a href="detail.html?name=${country.name}">
     <img
       src="${country.flags.png}"
       class="card-img-top"
       alt="..."
-    />
+    /></a>
     <div class="card-body">
-      <h5 class="card-title fw-bold fs-5">${country.name}</h5>
+      <h5 class="card-title fw-bold fs-5"> <a href="detail.html?name=${
+        country.name
+      }">${country.name}</a></h5>
       <p class="card-text mb-1">
-        <strong>Population:</strong> ${country.population}
+        <strong>Population:</strong> ${country.population.toLocaleString(
+          "en-US"
+        )}
       </p>
       <p class="card-text mb-1"><strong>Region</strong>: ${country.region}</p>
       <p class="card-text"><strong>Capital</strong>: ${country.capital}</p>
     </div>
   </div>`;
 };
+const displayCardsByRegion = (country) => {
+  return ` <div class="col-12 mb-3 col-sm-6 col-lg-3">
+  <div class="card">
+  <a href="detail.html?name=${country.name.common}">
+    <img
+      src="${country.flags.png}"
+      class="card-img-top"
+      alt="..."
+    />
+    </a>
+    <div class="card-body">
+      <h5 class="card-title fw-bold fs-5"> <a href="detail.html?name=${
+        country.name.common
+      }">${country.name.common}</a></h5>
+      <p class="card-text mb-1">
+        <strong>Population:</strong> ${country.population.toLocaleString(
+          "en-US"
+        )}
+      </p>
+      <p class="card-text mb-1"><strong>Region</strong>: ${country.region}</p>
+      <p class="card-text"><strong>Capital</strong>: ${country.capital}</p>
+    </div>
+  </div>`;
+};
+//* dark/light mode
+const darkmode = document.querySelector(".dark-mode");
+const toDarkMode = () => {
+  const header = document.querySelector(`header`);
+  header.classList.add("text-light", "bg-header");
+  const body = document.querySelector("body");
+  body.style.backgroundColor = `hsl(207, 26%, 17%)`;
+
+  lightmode.classList.remove("d-none");
+  darkmode.classList.add("d-none");
+  const input = document.getElementById("search-input");
+  input.style.backgroundColor = `hsl(209, 23%, 22%)`;
+  input.style.color = "hsl(0, 0%, 98%)";
+  const searchBox = document.querySelector(".search-box");
+  searchBox.classList.add("place");
+  const filter = document.querySelector(".dropdown > button");
+  filter.classList.remove("bg-white");
+  filter.classList.add("text-light");
+  filter.style.backgroundColor = `hsl(209, 23%, 22%)`;
+  const filterUl = document.querySelector(".dropdown > ul");
+  filterUl.style.backgroundColor = `hsl(209, 23%, 22%)`;
+  const liS = document.querySelectorAll("li > a");
+  const cards = document.querySelectorAll(".card");
+  console.log(cards);
+  cards.forEach((card) => {
+    card.style.backgroundColor = `hsl(209, 23%, 22%)`;
+    card.classList.add("text-light");
+  });
+  liS.forEach((li) => {
+    li.classList.add("text-light");
+  });
+  const bordersBtn = document.querySelectorAll("li button.rounded");
+  bordersBtn.forEach((el) => {
+    el.classList.remove("bg-white");
+    el.classList.add("text-white");
+  });
+};
+const lightmode = document.querySelector(".light-mode");
+const toLightMode = () => {
+  const header = document.querySelector(`header`);
+  header.classList.remove("text-light", "bg-header");
+  const body = document.querySelector("body");
+  body.style.backgroundColor = `hsl(0, 0%, 98%)`;
+
+  lightmode.classList.add("d-none");
+  darkmode.classList.remove("d-none");
+  const input = document.getElementById("search-input");
+  input.style.backgroundColor = `hsl(0, 0%, 98%)`;
+  input.style.color = "hsl(209, 23%, 22%)";
+  const searchBox = document.querySelector(".search-box");
+  searchBox.classList.remove("place");
+  const filter = document.querySelector(".dropdown > button");
+  filter.classList.add("bg-white");
+  filter.classList.remove("text-light");
+  filter.style.backgroundColor = `hsl(0, 0%, 98%)`;
+  const filterUl = document.querySelector(".dropdown > ul");
+  filterUl.style.backgroundColor = `hsl(0, 0%, 98%)`;
+  const liS = document.querySelectorAll("li > a");
+  const cards = document.querySelectorAll(".card");
+  console.log(cards);
+  cards.forEach((card) => {
+    card.style.backgroundColor = `hsl(0, 0%, 98%)`;
+    card.classList.remove("text-light");
+  });
+  liS.forEach((li) => {
+    li.classList.remove("text-light");
+  });
+  const bordersBtn = document.querySelectorAll("li button.rounded");
+  bordersBtn.forEach((el) => {
+    el.classList.add("bg-white");
+    el.classList.remove("text-white");
+  });
+};
+darkmode.addEventListener("click", toDarkMode);
+lightmode.addEventListener("click", toLightMode);
